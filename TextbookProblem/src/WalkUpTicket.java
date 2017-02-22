d743da8087154922f2b891071d2c0e193e59243d
kiimmyn <17ngk07@millshighsch.org>
2017-02-22 10:05:16 -0800

    hw2

--- TextbookProblem/src/WalkUpTicket.java
+++ TextbookProblem/src/WalkUpTicket.java
@@ -1,4 +1,25 @@
 
-public class WalkUpTicket {
-
+public class WalkUpTicket extends Ticket{
+	
+	int ticketNumber;
+	int ticketPrice;
+	
+	
+	public WalkupTicket (int ticketNumber){
+		super(ticketNumber);
+		ticketPrice = 50; 
+		//ticket price
+	}
+	
+	public double getPrice() { //price of ticket
+		return ticketPrice;
+	}
+	
+	public String toString() { //override String method
+		String walkUp = "Number: " + ticketNumber + ", Price : " + getPrice();
+		return walkUp;
+	}
+	
 }
+
+
