729dc119b0e5b27544300992bd9899f959aab423
kiimmyn <17ngk07@millshighsch.org>
2017-02-22 10:40:47 -0800

    temp

---
+++ Changing Temperature/src/Temperature.java
@@ -0,0 +1,11 @@
+import java.io.*; 
+import java.util.*;
+
+public class Temperature {
+	public static void main (String[] args) throws FileNotFoundException {
+	Scanner input= new Scanner(new File("./resources/weather.txt"));
+	String numbers = input.next();
+	while (input.hasNextDouble()){
+	}
+}
+	}
