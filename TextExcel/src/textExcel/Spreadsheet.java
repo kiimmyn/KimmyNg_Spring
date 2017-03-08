54ba708efc256079f7e04f3535f91355c3d2325d
kiimmyn <17ngk07@millshighsch.org>
2017-03-08 10:03:04 -0800

    textcell1

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -12,6 +12,34 @@ public class Spreadsheet implements Grid
 	@Override
 	public String processCommand(String command)
 	{
+		String[] splitted = command.split(" ");
+		if (splitted.equals("clear")) {
+			if (splitted.length > 6) {
+				// clears the given cell
+				Location clear= new SpreadsheetLocation(splitted[1]);
+				Cell change=getCell(clear);
+				change=new EmptyCell();
+			} else {
+				// clears the entire spreadsheet
+				excellSpreadsheet=new Cell [20][12];
+				for(int i=0; i<20; i++){
+					for(int j=0; j<12;j++){
+						excellSpreadsheet[i][j]=new EmptyCell();
+					}
+				}
+			}
+		} else if (splitted[1].equals("=")) {
+			Location equals = new SpreadsheetLocation(splitted[0]);
+			Cell change = getCell(equals);
+			change = new TextCell splitted[2]);
+			return getGridText();
+		} else if (splitted.length == 1) {
+			Location one = new SpreadsheetLocation(splitted[0]);
+			Cell n = getCell(one);
+			return n.fullCellText();
+		}
+		return command;
+	
 		// TODO Auto-generated method stub
 		return command;
 	}
---
+++ TextExcel/src/textExcel/TextCell.java
@@ -0,0 +1,23 @@
+package textExcel;
+
+public class TextCell implements Cell {
+	
+	private String content;
+	
+		public TextCell (String str){
+			str= content;
+		}
+
+	@Override
+	public String abbreviatedCellText() {
+		// TODO Auto-generated method stub
+		return content;
+	}
+
+	@Override
+	public String fullCellText() {
+		// TODO Auto-generated method stub
+		return content;
+	}
+
+}
